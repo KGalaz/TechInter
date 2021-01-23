71f68b3616b29fcda5de86d33a22a8dedcce802b
Konrad <konrad-galazka@outlook.com>
2021-01-23 13:35:53 +0100

    js dziala

--- src/main/java/techInter/demo/DemoApplication.java
+++ src/main/java/techInter/demo/DemoApplication.java
@@ -3,11 +3,11 @@ package techInter.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
-public class DemoApplication extends WebMvcAutoConfiguration {
+public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
--- src/main/resources/static/js/zamowienia.js
+++ src/main/resources/static/js/zamowienia.js
@@ -1,10 +1,39 @@
-let koszyk = [];
+const koszykMap = new Map;
 
-function dodajPotrawe(param) {
-    console.log(param)
-    // this.koszyk.push()
+function aktualizujKoszyk() {
+    //todo dokonczyc
+    let elemP = document.getElementById("koszyk");
+    let tekst;
+    koszykMap.forEach( (v, k) => {
+        tekst = tekst + k + ": ilość-> " + k + "<br>"
+    })
+    elemP.innerHTML = tekst;
 }
 
-function usunPotrawe() {
 
+
+function dodajPotrawe(param) {
+    let ilosc;
+    if(koszykMap.get(param) === undefined) {
+        ilosc = 0;
+    } else {
+        ilosc = koszykMap.get(param)
+    }
+    koszykMap.set(param, ++ilosc)
+    console.log("Koszyk: ", koszykMap)
+    aktualizujKoszyk();
+}
+
+function usunPotrawe(param) {
+    let ilosc;
+    if(koszykMap.get(param !== undefined)) {
+        return;
+    }
+    ilosc = koszykMap.get(param);
+    if(ilosc === 0) {
+        return;
+    }
+    koszykMap.set(param, --ilosc)
+    console.log(koszykMap)
+    aktualizujKoszyk()
 }
